adb046366c2c0bbf0f30ebee90fe169245934f6d
Fouad Nazir Ahmad Saleemi <55477002+Fouad-333@users.noreply.github.com>
2020-11-06 18:40:49 +1100

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,12 @@
+<?php
+
+//filename: [upload.html]
+//author: [Fouad Nazir Ahmad Saleemi]
+	
+	
+	$host = "Amazon_RDS_endpoint";
+	$user = "your_username";
+	$password = "your_password";
+	$db_name = "your_db_name";
+	
+?>
